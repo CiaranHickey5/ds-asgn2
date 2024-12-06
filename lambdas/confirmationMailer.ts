bda4a18ff9aa308c32001676fa62e6e3b37c8805
Ciaran <ciaran.hickey99@gmail.com>
2024-12-06 20:24:55 +0000

    Modify confirmationMailer lambda

--- lambdas/confirmationMailer.ts
+++ lambdas/confirmationMailer.ts
@@ -1,97 +1,54 @@
-import { SQSHandler } from "aws-lambda";
-import { SES_EMAIL_FROM, SES_EMAIL_TO, SES_REGION } from "../env";
+import { DynamoDBStreamHandler } from "aws-lambda";
 import {
   SESClient,
   SendEmailCommand,
   SendEmailCommandInput,
 } from "@aws-sdk/client-ses";
 
-type ContactDetails = {
-  name: string;
-  email: string;
-  message: string;
-};
+const client = new SESClient({ region: process.env.SES_REGION });
+const senderEmail = process.env.SES_EMAIL_FROM;
+const recipientEmail = process.env.SES_EMAIL_TO;
 
-const client = new SESClient({ region: SES_REGION });
+export const handler: DynamoDBStreamHandler = async (event) => {
+  if (!senderEmail || !recipientEmail) {
+    console.error("Sender or recipient email is not defined.");
+    throw new Error(
+      "SES_EMAIL_FROM or SES_EMAIL_TO environment variables are missing."
+    );
+  }
 
-export const handler: SQSHandler = async (event: any) => {
-  console.log("Event ", JSON.stringify(event));
   for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);
-    const snsMessage = JSON.parse(recordBody.Message);
+    if (record.eventName === "INSERT") {
+      const newImage = record.dynamodb?.NewImage;
+      if (newImage) {
+        const fileName = newImage.fileName.S;
 
-    if (snsMessage.Records) {
-      console.log("Record body ", JSON.stringify(snsMessage));
-      for (const messageRecord of snsMessage.Records) {
-        const s3e = messageRecord.s3;
-        const srcBucket = s3e.bucket.name;
-        // Object key may have spaces or unicode non-ASCII characters.
-        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+        const params: SendEmailCommandInput = {
+          Destination: {
+            ToAddresses: [recipientEmail],
+          },
+          Message: {
+            Body: {
+              Html: {
+                Charset: "UTF-8",
+                Data: `<p>A new image with fileName <strong>${fileName}</strong> has been added to the album.</p>`,
+              },
+            },
+            Subject: {
+              Charset: "UTF-8",
+              Data: "New Image Added Notification",
+            },
+          },
+          Source: senderEmail,
+        };
+
         try {
-          const { name, email, message }: ContactDetails = {
-            name: "The Photo Album",
-            email: SES_EMAIL_FROM,
-            message: `We received your Image. Its URL is s3://${srcBucket}/${srcKey}`,
-          };
-          const params = sendEmailParams({ name, email, message });
           await client.send(new SendEmailCommand(params));
-        } catch (error: unknown) {
-          console.log("ERROR is: ", error);
-          // return;
+          console.log(`Email notification sent for new image: ${fileName}`);
+        } catch (error) {
+          console.error("Error sending email notification:", error);
         }
       }
     }
   }
 };
-
-function sendEmailParams({ name, email, message }: ContactDetails) {
-  const parameters: SendEmailCommandInput = {
-    Destination: {
-      ToAddresses: [SES_EMAIL_TO],
-    },
-    Message: {
-      Body: {
-        Html: {
-          Charset: "UTF-8",
-          Data: getHtmlContent({ name, email, message }),
-        },
-        // Text: {.           // For demo purposes
-        //   Charset: "UTF-8",
-        //   Data: getTextContent({ name, email, message }),
-        // },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: `New image Upload`,
-      },
-    },
-    Source: SES_EMAIL_FROM,
-  };
-  return parameters;
-}
-
-function getHtmlContent({ name, email, message }: ContactDetails) {
-  return `
-    <html>
-      <body>
-        <h2>Sent from: </h2>
-        <ul>
-          <li style="font-size:18px">👤 <b>${name}</b></li>
-          <li style="font-size:18px">✉️ <b>${email}</b></li>
-        </ul>
-        <p style="font-size:18px">${message}</p>
-      </body>
-    </html> 
-  `;
-}
-
-// For demo purposes - not used here.
-function getTextContent({ name, email, message }: ContactDetails) {
-  return `
-    Received an Email. 📬
-    Sent from:
-        👤 ${name}
-        ✉️ ${email}
-    ${message}
-  `;
-}
